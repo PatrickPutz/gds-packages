8158a6d15a61485e1f1a5ce2461deb2f3004181f
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-06 13:46:22 +0100

    added demo stack with strings

--- src/org/campus02/putz/stack/DemoStackApp.java
+++ src/org/campus02/putz/stack/DemoStackApp.java
@@ -3,7 +3,7 @@ package org.campus02.putz.stack;
 public class DemoStackApp {
     public static void main(String[] args) {
 
-        Stack stapel = new Stack(new Integer[7]);
+        Stack<Integer> stapel = new Stack(new Integer[7]);
 
         stapel.push(7);
         stapel.push(5);
@@ -15,6 +15,19 @@ public class DemoStackApp {
         System.out.println("stapel.pop() = " + stapel.pop());
         System.out.println("stapel.pop() = " + stapel.pop());
         System.out.println("stapel.pop() = " + stapel.pop());
+
+        Stack<String> stapel2 = new Stack(new String[7]);
+
+        stapel2.push("Sieben");
+        stapel2.push("Fünf");
+        stapel2.push("Sechs");
+
+        System.out.println("stapel2.pop() = " + stapel2.pop());
+        System.out.println("stapel2.pop() = " + stapel2.pop());
+        stapel2.push("Drei");
+        System.out.println("stapel2.pop() = " + stapel2.pop());
+        System.out.println("stapel2.pop() = " + stapel2.pop());
+        System.out.println("stapel2.pop() = " + stapel2.pop());
 
     }
 }
